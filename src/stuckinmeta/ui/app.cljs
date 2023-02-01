32fc1726e3d37eb2227d10758b2e97e9a3935eef
Rafal Dittwald <rafal.dittwald@gmail.com>
2023-02-01 00:10:00 -0500

    Make layout mobile friendly

--- src/stuckinmeta/ui/app.cljs
+++ src/stuckinmeta/ui/app.cljs
@@ -23,29 +23,31 @@
                              (reset! image (rand-nth (vec (disj (set images) @image)))))
                _ (reset! timeout (js/setTimeout swap-image! 15000))]
     (let [[color-1 color-2 color-3 color-4] (:image/colors @image)]
-      [:div
+      [:div {:style {:background-color color-1
+                     :transition "background-color linear 2s"}}
        [:style "@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap');
                p { font-family: 'VT323', monospace; font-size: 2em; font-variant-ligatures: none;}
                p {line-height: 1.1}
-               button, h1 { font-family: 'Press Start 2P', monospace;}"]
+               button, h1 { font-family: 'Press Start 2P', monospace;}
+               .bg { background-position: 22.5% bottom}
+               @media (min-width: 1024px) {
+                 .bg { background-position: left bottom}
+               }
+               "]
        [:section.hero
-        [:div {:tw "fixed top-0 h-screen w-screen"
-               :style {:background-image (str "url(" (:image/path @image) ")")
-                       :background-size "contain"
-                       :background-color color-1
-                       :background-repeat "no-repeat"
-                       :flex-basis "50vw"
-                       :aspect-ratio 1
-                       :transition "background-color linear 2s, background-image linear 2s"}
-               :on-click (fn [_]
-                           (swap-image!))}]
-        [:div.content {:tw "p-12 space-y-12 max-w-58em ml-auto z-10 relative"}
-         [:h1 {:tw "font-size-3em"
-               :style {;:position "absolute"
-                       ;:text-shadow (str "2px 2px " color-1)
-                       ;:top 0
-                       ;:left 0
-                       :background color-1
+
+        [:div.bg {:tw ["w-screen h-100vh lg:h-screen"
+                       "fixed top-0"
+                       "bg-no-repeat bg-cover lg:bg-contain lg:bg-bottom"]
+                  #_(Math/round (* 100 (/ 1606 1024)))
+                  :style {:background-image (str "url(" (:image/path @image) ")")
+                          :background-color color-1
+                          :transition "background-color linear 2s, background-image linear 2s"}
+                  :on-click (fn [_]
+                              (swap-image!))}]
+        [:div.content {:tw ["md:p-12 p-6 space-y-12 max-w-58em ml-auto z-10 relative mt-420px md:mt-0"]}
+         [:h1 {:tw "font-size-3em "
+               :style {:background color-1
                        :padding "0.5em"
                        :display "inline-block"
                        :color color-2
@@ -63,12 +65,12 @@
           [:p {:tw "p-2 inline-block"
                :style {:background color-1
                        :transition "background linear 2s"}}
-           "Godzilla is rampaging across Middle Earth!" [:br] "The Xenomorph has found its way onto the Battlestar Galactica?"]
-          [:p {:tw "ml-12 p-2 inline-block"
+           "Godzilla is rampaging across Middle Earth!" [:br] "The Xenomorph has found its way onto the Battlestar Galactica?!"]
+          [:p {:tw "md:ml-12 p-2 inline-block"
                :style {:background color-1
                        :transition "background linear 2s"}}
            "A corruption is spreading through the metaverse, and " [:br] "someone needs to fix it! Will it be you?"]
-          [:p {:tw "ml-24 p-2 inline-block"
+          [:p {:tw "md:ml-24 p-2 inline-block"
                :style {:background color-1
                        :transition "background linear 2s"}}
            "No, not you! That's ridiculous. The Overmind will handle it. After all, it runs this place." " ...but, until it figures things out, we need someone to patch those rogue servers. One by one. Might as well be you."]]
@@ -80,15 +82,15 @@
                :style {:background color-1
                        :transition "background linear 2s"}}
            "Stuck in Meta is a massive multigroup ttRPG campaign, powered by Monster of the Week. It's DnD meets Westworld meets the Metaverse, in a continously evolving open universe of parallel adventures."]
-          [:p {:tw "ml-12 p-2 inline-block"
+          [:p {:tw "md:ml-12 p-2 inline-block"
                :style {:background color-1
                        :transition "background linear 2s"}}
            "Build a character. Join a one-off session (or run your own)." [:br] "Make some friends. Have some laughs. Slay some monsters."]
-          [:p {:tw "ml-24 text-right p-2 inline-block"
+          [:p {:tw "md:ml-24 text-right p-2 inline-block"
                :style {:background color-1
                        :transition "background linear 2s"}}
            "...because the metaverse needs you."]]
-         [:button {:tw "ml-36 border border-4 p-4"
+         [:button {:tw "ml-36 border border-4 p-4 whitespace-nowrap"
                    :style {:color color-2
                            :background color-1
                            :border-color color-2
